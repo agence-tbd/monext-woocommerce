f730d4ca75227fb6caa61e7da770829d90576a25
Vincent Pietri <vincent.pietri@tbdgroup.com>
2024-09-24 11:52:16 +0200

    Version 1.5.1

--- includes/gateway/class-wc-gateway-abstract-payline.php
+++ includes/gateway/class-wc-gateway-abstract-payline.php
@@ -18,7 +18,7 @@ abstract class WC_Abstract_Payline extends WC_Payment_Gateway {
 
     protected $paymentMode = '';
 
-    protected $extensionVersion = '1.5.0';
+    protected $extensionVersion = '1.5.1';
 
     /** @var int Payline internal API version */
     protected $APIVersion = 26;
